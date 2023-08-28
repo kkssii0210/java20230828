611a5bc41fdaefd61db4357dc40eccafe866a4b1
kkssii0210 <kkssii0210@naver.com>
2023-08-28 12:35:54 +0900

    오전에 공부한 내용

--- src/ch05reference/C01primitive.java
+++ src/ch05reference/C01primitive.java
@@ -3,8 +3,17 @@ package ch05reference;
 public class C01primitive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String a = "java";
+		String b = "spring";
+		String c = a;
+		
+		int code1 =System.identityHashCode(a);
+		int code2 = System.identityHashCode(b);
+		int code3 = System.identityHashCode(c);
+		System.out.println(code1);
+		System.out.println(code2);
+		System.out.println(code3);
+		
+	
 	}
-
 }
---
+++ src/ch05reference/C09method.java
@@ -0,0 +1,29 @@
+package ch05reference;
+
+public class C09method {
+
+	public static void main(String[] args) {
+		int a = 5;
+		method1(a);
+		int b = 50;
+		method1(b);
+		int c[] = {3,4,5};
+		System.out.println("메인에서");
+		System.out.println(System.identityHashCode(c));
+		method2(c);
+		System.out.println("메소드 호출 후");
+		System.out.println(c[0]);
+
+	}
+	public static void method1(int p) {
+		System.out.println(p);
+	}
+	public static void method2(int p[]) {
+		System.out.println("메소드 1에서");
+		System.out.println(System.identityHashCode(p));
+		System.out.println(p[0]);
+		
+		p[0]=33;
+	}
+
+}
